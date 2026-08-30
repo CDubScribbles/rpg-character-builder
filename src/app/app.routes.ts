d2d4e6892a851fe330fb8d6853a2dc123de338c3
EMFDeathPenguin <cliffordwsmith@gmail.com>
2026-08-30 16:15:34 -0400

    Initial commit: RPG Character Builder through Lab 3.1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClassesComponent } from './classes/classes.component';
+import { ClassDetailComponent } from './class-detail/class-detail.component';
+import { AboutComponent } from './about/about.component';
+import { AbilityRollerComponent } from './ability-roller/ability-roller.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'classes', component: ClassesComponent },
+  { path: 'classes/:id', component: ClassDetailComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: { title: 'About RPG Character Builder' }
+  },
+  { path: 'roll/:sides', component: AbilityRollerComponent }
+];
